b6944cb0a3d89f9281d1053057d35f6eb5a41d69
Marius Larsen <marius.b.larsen@live.no>
2019-03-12 13:45:30 +0100

    Subplots added. With and without feedback

--- Problem_10_4_3/plotter_wof.m
+++ Problem_10_4_3/plotter_wof.m
@@ -12,8 +12,8 @@ title('Optimal trajectory without feedback - Pitch $p$')
 ylabel('Pitch angle [rad]');
 xlabel('Time [s]');
 legend({'Pitch reference $p_c$', 'Pitch measured $p$'});
-saveas(gcf, 'problem_10_4_plot_pitch','epsc');
-savefig('problem_10_4_plot_pitch');
+saveas(gcf, 'problem_10_4_wof_plot_pitch','epsc');
+savefig('problem_10_4_wof_plot_pitch');
 
 figure
 plot(t,x1)
@@ -23,8 +23,8 @@ title('Optimal trajectory without feedback - Travel $\lambda$')
 ylabel('Travel angle [rad]');
 xlabel('Time [s]');
 legend({'Travel reference $\lambda_c$', 'Travel measured $\lambda$'});
-saveas(gcf, 'problem_10_4_plot_travel','epsc');
-savefig('problem_10_4_plot_travel');
+saveas(gcf, 'problem_10_4_wof_plot_travel','epsc');
+savefig('problem_10_4_wof_plot_travel');
 
 e_constraint = nonlincon_lambda(simout(2,:));
 
@@ -37,5 +37,5 @@ ylabel('Elevation angle [rad]');
 xlabel('Time [s]');
 ylim([-0.5 0.25])
 legend({'Elevation reference $e_c$', 'Elevation measured $e$', 'Lower elevation constraint'});
-saveas(gcf, 'problem_10_4_plot_elevation','epsc');
-savefig('problem_10_4_plot_elevation');
+saveas(gcf, 'problem_10_4_wof_plot_elevation','epsc');
+savefig('problem_10_4_wof_plot_elevation');
